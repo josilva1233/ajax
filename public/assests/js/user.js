74bc19680c9fadeb456153f595f0dcbdb6b8948f
Josias Silva <josilva1233@gmail.com>
2024-06-11 15:38:21 -0300

    mundando para this a  função js

--- public/assests/js/user.js
+++ public/assests/js/user.js
@@ -5,8 +5,9 @@ window.onload = function() {
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            console.log(xhttp.responseText);
+
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
         }
     }
 
