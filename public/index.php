30566cf24dc1d5fb42035cdfcc41deb853f9d194
josias Silva <josilva1233@gmail.com>
2024-06-12 10:58:33 -0300

    Refatorando  estrutura ajax

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
 
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+
 </head>
 <body>
     <div class="container">
@@ -16,6 +16,7 @@
            <div id="div-users"></div>
         </div>
     </div>  
+    <script src="assests/js/xhttp.js"></script>
    <script src="assests/js/user.js"></script>
 </body>
 </html>
